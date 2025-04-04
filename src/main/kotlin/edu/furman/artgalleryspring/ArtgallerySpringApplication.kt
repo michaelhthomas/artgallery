16f7219d64c4cbbbd56fea5ec7df06b7326d9f76
Michael Thomas <michaelhthomas@outlook.com>
2025-04-04 17:17:58 -0400

    feat: initialize basic project with db connection

---
+++ src/main/kotlin/edu/furman/artgalleryspring/ArtgallerySpringApplication.kt
@@ -0,0 +1,11 @@
+package edu.furman.artgalleryspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ArtgallerySpringApplication
+
+fun main(args: Array<String>) {
+    runApplication<ArtgallerySpringApplication>(*args)
+}
---
+++ src/test/kotlin/edu/furman/artgalleryspring/ArtgallerySpringApplicationTests.kt
@@ -0,0 +1,13 @@
+package edu.furman.artgalleryspring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ArtgallerySpringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
